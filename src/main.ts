d0e4d001e364450a5f6ddef1e21e8ede88c68d84
ahmed-adam-dev <ahmedadammohammed.abdullah@gmail.com>
2025-06-13 21:18:13 +0300

    switch to  prisma client direct instead of generated prisma

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Resale Api')
+    .setDescription('API documentation for resale')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document); // Docs available at /api-docs
+
   await app.listen(process.env.PORT ?? 3000);
 }
-bootstrap();
+void bootstrap();
