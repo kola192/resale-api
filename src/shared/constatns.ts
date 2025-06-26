760fa0fe03a3ba9a845245da4c7dc747e463b4a6
ahmed-adam-dev <ahmedadammohammed.abduallah@gmail.com>
2025-06-26 00:02:57 +0000

    Remove the slash to fix the CORS bug
    Signed-off-by: ahmed-adam-dev <ahmedadammohammed.abduallah@gmail.com>

--- src/shared/constatns.ts
+++ src/shared/constatns.ts
@@ -1,3 +1,3 @@
-export const originList = ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://resale-website.netlify.app/'];
+export const originList = ['http://localhost:5173', 'http://127.0.0.1:5173', 'http://localhost:5174', 'http://127.0.0.1:5174', 'https://resale-website.netlify.app'];
 
 export const PORT = 3054;
